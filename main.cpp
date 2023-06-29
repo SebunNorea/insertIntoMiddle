4d151d973cbbb1739ca805608b68aae34b7f8a9b
SebunNorea <122924969+SebunNorea@users.noreply.github.com>
2023-06-29 21:28:26 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    void insertIntoMiddle (int value, int pos) {
+        // Создаем новый узел
+        Node* newNode = new Node;
+        newNode->value = value;
+        newNode->next = nullptr;
+        newNode->node_mutex = new mutex();
+
+        // Захватываем глобальную блокировку очереди
+        queue_mutex->lock();
+
+        // Перемещаемся на позицию pos - 1
+        Node* current = head;
+        int count = 0;
+        while (current->next && count < pos - 1) {
+            current = current->next;
+            count++;
+        }
+
+        // Если позиция pos больше длины списка, вставляем элемент в конец
+        if (count < pos - 1) {
+            current->next = newNode;
+        }
+        else {
+            // Захватываем блокировку текущего узла и следующего за ним
+            current->node_mutex->lock();
+            Node* nextNode = current->next;
+            nextNode->node_mutex->lock();
+
+            // Вставляем новый узел в середину списка
+            newNode->next = nextNode;
+            current->next = newNode;
+
+            // Освобождаем блокировки
+            nextNode->node_mutex->unlock();
+            current->node_mutex->unlock();
+        }
+
+        // Освобождаем глобальную блокировку очереди
+        queue_mutex->unlock();
+    }
